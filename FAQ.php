d4ec777470779cf89ae6f9508eeff07693d686fd
Olaoluwa Jaiyeola <55296977+JayGee-Francis@users.noreply.github.com>
2019-10-05 04:34:06 -0700

    Update FAQ.php
    Sorry for spamming. I noticed another slight error.

--- FAQ.php
+++ FAQ.php
@@ -80,5 +80,7 @@
                 <hr/>
                 <p align="right">Want to know about the programmers who developed Pontus? Check them out <a href="about.php", target="_blank">here</a>.
         </font>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
     </body>
 </html>
